3d06ac1d1bb499fd48657e9985853865b49c72ee
DanTheDev-zsh <XuDanDev@gmail.com>
2026-08-28 17:13:15 -0700

    did binary search using a cond block

--- line-up/lib/line_up.ex
+++ line-up/lib/line_up.ex
@@ -1,9 +1,22 @@
 defmodule LineUp do
+  @moduledoc """
+  Exercism exercise - Line up
+  """
   @doc """
   Formats a full ticket sentence for the given name and number, including
   the person's name, the ordinal form of the number, and fixed descriptive text.
   """
   @spec format(name :: String.t(), number :: pos_integer()) :: String.t()
-  def format(name, number) do
+  def format(name, number) when number in 1..999 do
+    integer_suffix =
+      cond do
+        rem(number, 100) in 11..13 -> "th"
+        rem(number, 10) == 1 -> "st"
+        rem(number, 10) == 2 -> "nd"
+        rem(number, 10) == 3 -> "rd"
+        true -> "th"
+      end
+
+    "#{name}, you are the #{number}#{integer_suffix} customer we serve today. Thank you!"
   end
 end
