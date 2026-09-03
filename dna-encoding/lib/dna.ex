f4e2c6874141a4add5d2bacb0dee07eb94e895bb
DanTheDev-zsh <XuDanDev@gmail.com>
2026-09-02 19:51:49 -0700

    this dna encoding problem is cool, I got stuck when my environment neovim's neotest and ExUnit couldn't agree on test id because it didn't handle escape sequence correctly. I also don't like the fact that the syllabus for this problem bans Enum. Enum.reverse is required for decoding. I made my own but its not optimized as BIF erlang Enum.

--- dna-encoding/lib/dna.ex
+++ dna-encoding/lib/dna.ex
@@ -1,17 +1,53 @@
 defmodule DNA do
-  def encode_nucleotide(code_point) do
-    # Please implement the encode_nucleotide/1 function
+  @moduledoc """
+  Exercism exercise - DNA encoding
+  """
+
+  def encode_nucleotide(?\s), do: 0b0000
+  def encode_nucleotide(?A), do: 0b0001
+  def encode_nucleotide(?C), do: 0b0010
+  def encode_nucleotide(?G), do: 0b0100
+  def encode_nucleotide(?T), do: 0b1000
+
+  def decode_nucleotide(0b0000), do: ?\s
+  def decode_nucleotide(0b0001), do: ?A
+  def decode_nucleotide(0b0010), do: ?C
+  def decode_nucleotide(0b0100), do: ?G
+  def decode_nucleotide(0b1000), do: ?T
+
+  def encode(charlist), do: do_encode(charlist, <<>>)
+
+  defp do_encode([], bitstring), do: bitstring
+
+  defp do_encode([head | tail], bitstring) do
+    encoded = encode_nucleotide(head)
+    do_encode(tail, <<bitstring::bitstring, encoded::4>>)
   end
 
-  def decode_nucleotide(encoded_code) do
-    # Please implement the decode_nucleotide/1 function
+  # def decode(bitstring), do: do_decode(bitstring, ~c"")
+  #
+  # defp do_decode(<<>>, charlist), do: Enum.reverse(charlist)
+  #
+  # defp do_decode(<<first::4, rest::bitstring>>, charlist) do
+  #   decoded = decode_nucleotide(first)
+  #   do_decode(rest, [decoded | charlist])
+  # end
+
+  # def decode(<<>>), do: ~c""
+  #
+  # def decode(<<first::4, rest::bitstring>>) do
+  #   [decode_nucleotide(first) | decode(rest)]
+  # end
+
+  def decode(dna), do: do_decode(dna, ~c"")
+
+  defp do_decode(<<>>, acc), do: reverse(acc, ~c"")
+
+  defp do_decode(<<first::4, rest::bitstring>>, acc) do
+    decoded = decode_nucleotide(first)
+    do_decode(rest, [decoded | acc])
   end
 
-  def encode(dna) do
-    # Please implement the encode/1 function
-  end
-
-  def decode(dna) do
-    # Please implement the decode/1 function
-  end
+  defp reverse([], acc), do: acc
+  defp reverse([head | tail], acc), do: reverse(tail, [head | acc])
 end
